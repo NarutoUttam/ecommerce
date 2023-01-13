429c98387d99218b1440c356a8bea77c43f06834
NarutoUttam <07071997.ups@gmail.com>
2023-01-13 19:20:06 +0530

    e-commerce design

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "images/img1.png",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "images/img2.png",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "images/img3.png",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4"
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "images/img5.jpg",
+        title: "SHIRT STYLE",
+    },
+    {
+        id: 2,
+        img: "images/img15.jpg",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "images/img7.jpg",
+        title: "LIGHT JACKETS",
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "images/img21.png",
+    },
+    {
+        id: 2,
+        img: "images/img22.png",
+    },
+    {
+        id: 3,
+        img: "images/img23.png",
+    },
+    {
+        id: 4,
+        img: "images/img24.png",
+    },
+    {
+        id: 5,
+        img: "images/img25.png",
+    },
+    {
+        id: 6,
+        img: "images/img26.png",
+    },
+    {
+        id: 7,
+        img: "images/img27.png",
+    },
+    {
+        id: 8,
+        img: "images/img28.png",
+    },
+]
